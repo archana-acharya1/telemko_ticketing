41f0c70413de5066f26d2b6fc309b9b3ea35852c
Archana Acharya <aacharyaarchana057@gmail.com>
2026-01-22 17:09:42 +0545

    wip

--- lib/presentation/screens/auth/auth_gate.dart
+++ lib/presentation/screens/auth/auth_gate.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:telemko_support/presentation/screens/auth/sms_auth_screen.dart';
 import 'package:telemko_support/presentation/screens/dashboard/dashboard_screen.dart';
 import 'package:telemko_support/presentation/screens/dashboard/lib/data/local/session_manager.dart';
+import 'package:telemko_support/presentation/screens/navbar/main_navbar.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
@@ -9,7 +10,8 @@ class AuthGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (SessionManager.isLoggedIn()) {
-      return const DashboardScreen();
+      // return const DashboardScreen();
+      return const MainNavbar();
     } else {
       return const SmsAuthScreen();
     }
